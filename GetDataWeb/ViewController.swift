b5bfd7e48672ea629cfd3cbea82e7a3baa2b7eed
farhan hussain <farhanleo23@gmail.com>
2018-10-27 13:48:50 +0200

    add UITextField and button

--- GetDataWeb/ViewController.swift
+++ GetDataWeb/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func getData(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var textField: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
